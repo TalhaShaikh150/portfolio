c4c2e7719a30e636f96dae885b67c346dc1049c1
TalhaShaikh150 <tssmart233@gmail.com>
2025-07-12 12:38:44 +0500

    Fix bugs, update contact details, and add backend folder for form handling

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Initialize particles.js
+import { projects } from "./projects.js";
+
 document.addEventListener("DOMContentLoaded", function () {
   // Loading screen
   setTimeout(() => {
@@ -85,72 +87,7 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   ];
 
-  const projects = [
-    {
-      id: 1,
-      image: "assets/code-studio.png",
-      link: "https://talhashaikh150.github.io/Code-Editor/",
-      name: "Code Studio Editor",
-      description:
-        "A web-based editor with HTML, CSS, and JS panels, live JS execution, fullscreen and built-in boilerplate setup.",
-      github: "https://github.com/TalhaShaikh150/Code-Editor",
-      tech: ["HTML", "CSS", "JavaScript"],
-    },
-    {
-      id: 2,
-      image: "assets/instagram.png",
-      link: "https://talhashaikh150.github.io/Instagram-Clone/",
-      name: "Instagram Clone",
-      description:
-        "Built with HTML, CSS, JS, and Supabase. Users can upload profile images, create posts, and view a shared feed with the latest updates.",
-      github: "https://github.com/TalhaShaikh150/Instagram-Clone",
-      tech: ["HTML", "CSS", "JavaScript", "Supabase"],
-    },
-    {
-      id: 3,
-      image: "assets/notes.png",
-      link: "https://talhashaikh150.github.io/Notes-App/",
-      name: "Notes App",
-      description:
-        "Notes website with category filtering, search, edit, and theme switcher. Built with HTML, CSS, JS, and local storage support.",
-      github: "https://github.com/TalhaShaikh150/Notes-App",
-      tech: ["HTML", "CSS", "JavaScript"],
-    },
-
-    {
-      id: 4,
-      image: "assets/css-tools.png",
-      link: "https://talhashaikh150.github.io/Css-Tools/",
-      name: "Css Tools",
-      description:
-        "A live CSS tool to customize box shadows with real-time preview and copyable code. Built with HTML, CSS, and JavaScript.",
-      github: "hhttps://github.com/TalhaShaikh150/Css-Tools",
-      tech: ["HTML", "CSS", "JavaScript"],
-    },
-    {
-      id: 5,
-      image: "assets/e-commerce.png",
-      link: "https://talhashaikh150.github.io/E-Commerce/",
-      name: "LuxuryFits UI",
-      description:
-        "A stylish watch store UI with add-to-cart feature. Focused on clean design and smooth user experience.",
-      github: "https://github.com/TalhaShaikh150/E-Commerce",
-      tech: ["HTML", "CSS", "JavaScript"],
-    },
-    {
-      id: 6,
-      image: "assets/agent-chain.png",
-      link: "https://agent-chain-builder.vercel.app/",
-      name: "Agent Chain UI",
-      description:
-        "Built with Next.js and Tailwind CSS. A responsive, user-friendly interface for managing AI agent workflows.",
-      github: "https://github.com/TalhaShaikh150/agent-chain-builder",
-      tech: ["Next.js", "Tailwind Css"],
-    },
-  ];
-
   // Render projects
-  const container = document.getElementById("projects-container");
   const clientContainer = document.getElementById("client-projects-container");
 
   clientProjects.forEach((project) => {
@@ -181,6 +118,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     clientContainer.appendChild(clientProjectEl);
   });
+
+  const container = document.getElementById("projects-container");
 
   projects.forEach((project) => {
     const projectEl = document.createElement("div");
@@ -392,3 +331,13 @@ document.addEventListener("DOMContentLoaded", function () {
     initThreeJS();
   }, 2000);
 });
+
+function getCurrentDate() {
+  let today = new Date();
+  const currentYearElement = document.querySelector(".current-year");
+
+  const currentYear = today.getFullYear();
+  currentYearElement.innerHTML = currentYear;
+}
+
+// getCurrentDate();
